2363c01fb1896c94f7fdc815b937ff39699da139
lazarljubenovic <lazar@weareneopix.com>
2018-06-27 12:54:45 +0200

    docs: add some docs to some functions

--- src/index.ts
+++ src/index.ts
@@ -36,15 +36,86 @@ export function isOfBasicType<T extends BasicString> (basicString: T): Guard<Str
   return ((input: any) => typeof input == basicString) as Guard<StringToBasic<T>>
 }
 
+/**
+ * Create a validator that asserts the passed argument is exactly `null`,
+ * just like `input === null`.
+ * @type {(input: any) => input is any}
+ */
 export const isNull = is(null)
+
+/**
+ * Create a validator that asserts the passed argument is exactly `undefined`,
+ * just like `input === undefined`.
+ * @type {(input: any) => input is any}
+ */
 export const isUndefined = is(undefined)
+
+/**
+ * Create a validator that asserts the passed argument is either `null` or `undefined`,
+ * just like `input == null`.
+ * @type {Guard<any>}
+ */
 export const isNullOrUndefined = oneOf(isNull, isUndefined)
+
+/**
+ * Create a validator that asserts the passed argument is of type `'number'`,
+ * just like `typeof input == 'number'`.
+ * @type {Guard<StringToBasic<"number">>}
+ */
 export const isNumber = isOfBasicType('number')
+
+/**
+ * Create a validator that asserts the passed argument is of type `'string'`,
+ * just like `typeof input == 'string'`.
+ * @type {Guard<StringToBasic<"string">>}
+ */
 export const isString = isOfBasicType('string')
+
+/**
+ * Create a validator that asserts the passed argument is of type `'boolean'`,
+ * just like `typeof input == 'boolean'`.
+ * @type {Guard<StringToBasic<"boolean">>}
+ */
 export const isBoolean = isOfBasicType('boolean')
+
+/**
+ * Create a validator that asserts the passed argument is of type `'function'`,
+ * just like `typeof input == 'function'`.
+ *
+ * @type {Guard<StringToBasic<"function">>}
+ */
 export const isFunction = isOfBasicType('function')
+
+/**
+ * Create a validator that asserts the passed argument is of type `'object'`,
+ * just like `typeof input == 'object'`.
+ *
+ * You are probably looking for `isOfShape` which lets you specify what kind of
+ * keys and values should the object consist of.
+ *
+ * @type {Guard<StringToBasic<"object">>}
+ */
 export const isObject = isOfBasicType('object')
 
+/**
+ * Create a validator that asserts the passed argument is one of the given values.
+ * Usually used for enumerations, but does not require the elements of enumeration
+ * to be of the same type.
+ *
+ * @param enums
+ */
+export function isEnum (...enums: Basic[]) {
+  return (input: any): boolean => {
+    return enums.some(enumConst => enumConst === input)
+  }
+}
+
+/**
+ * Create a validator that asserts that passed argument is an array. Accepts another
+ * type guard which is used for every item of the array.
+ *
+ * @param itemGuard
+ */
 export function isArrayOf<T> (itemGuard: Guard<T>): Guard<T[]> {
   return ((input: any) => Array.isArray(input) && input.every(itemGuard)) as Guard<T[]>
 }
@@ -53,6 +124,11 @@ export type Dict = { [key: string]: any }
 
 export type Shape<T extends Dict> = { [key in keyof T]: T[key] extends Dict ? Shape<T[key]> : Guard<T> }
 
+/**
+ * Create a validator that asserts that passed argument is an object of a certain shape.
+ * Accepts an object of guards.
+ * @param shape
+ */
 export function isOfShape<T extends { [key: string]: any }> (shape: Shape<T>): Guard<T> {
   return function (input: any): input is T {
     if (typeof input != 'object') return false
@@ -64,11 +140,5 @@ export function isOfShape<T extends { [key: string]: any }> (shape: Shape<T>): G
         return isOfShape(keyGuard)(input[key])
       }
     })
-  }
-}
-
-export function isEnum (...enums: Basic[]) {
-  return (input: any): boolean => {
-    return enums.some(enumConst => enumConst === input)
   }
 }
